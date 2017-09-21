706b9ff15da07b52c414e2c5be4fb805281de8dd
Lee Zamparo <zamparo@gmail.com>
2017-09-21 16:41:08 -0400

    working towards split guides jobscript

--- src/split_guides.py
+++ src/split_guides.py
@@ -27,6 +27,14 @@ def new_part_file(infile, partition_number, header):
 # split the infile into each piece file, write out the guides
 partition_num = 0
 part_file = new_part_file(infile, partition_num, header)
+print(header, file=part_file)
+
 for count, guide in lines[1:]:
-    
-    
+    print(guide, file=part_file)
+    if count > lines_per_partition:
+        part_file.close()
+        partition_num = partition_num + 1
+        part_file = new_part_file(infile, partition_number, header)
+
+# close the remainind partition file
+part_file.close()
