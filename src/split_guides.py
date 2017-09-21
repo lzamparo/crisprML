ec96e87906c78603a59ca66fce437b537c7694cb
Lee Zamparo <zamparo@gmail.com>
2017-09-21 18:12:43 -0400

    fixed bug in testing for new file opening

--- src/split_guides.py
+++ src/split_guides.py
@@ -6,14 +6,13 @@ import sys
 infile = sys.argv[1]
 partitions = sys.argv[2]
 
-
 # read the lines
 with open(infile, 'r') as f:
     lines = f.readlines()
 
 # calculate the load for each file
-lines_per_partition = len(lines) - 1 // int(partitions)
-header = lines[0]
+lines_per_partition = len(lines[1:]) // int(partitions)
+header = lines[0].strip()
 
 # open a new partition file:
 def new_part_file(infile, partition_number, header):
@@ -28,13 +27,16 @@ def new_part_file(infile, partition_number, header):
 partition_num = 0
 part_file = new_part_file(infile, partition_num, header)
 print(header, file=part_file)
+print("Working on partition ", partition_num)
 
 for count, guide in enumerate(lines[1:]):
+    guide = guide.strip()
     print(guide, file=part_file)
     if count > lines_per_partition:
         part_file.close()
         partition_num = partition_num + 1
         part_file = new_part_file(infile, partition_number, header)
+        print("Working on partition ", partition_num)
 
 # close the remainind partition file
 part_file.close()
