e1d21b238c5572000ad8bd043317f6dabc6df339
Lee Zamparo <zamparo@gmail.com>
2017-10-31 14:00:58 -0400

    adding script to merge guide data per organism

---
+++ src/both_screens_to_annotated_guides.py
@@ -0,0 +1,22 @@
+import pandas
+import os
+
+# load both data sets
+os.chdir(os.path.expanduser("~/projects/crisprML/data/"))
+
+hg_table = pandas.read_csv("raw_features_computed_Cas9_sequences_vs_hg19.csv")
+mm_table = pandas.read_csv("raw_features_computed_Cas9_sequences_vs_mm10.csv")
+
+# cut down levenstein dist, summed stuff
+hg_table = hg_table[['sequence','Specificity_Score','Occurrences_at_Hamming_0','Occurrences_at_Hamming_1', 'Occurrences_at_Hamming_2','Occurrences_at_Hamming_3','Sum_Hamming_Neighbors']]
+mm_table = mm_table[['sequence','Specificity_Score','Occurrences_at_Hamming_0','Occurrences_at_Hamming_1', 'Occurrences_at_Hamming_2','Occurrences_at_Hamming_3','Sum_Hamming_Neighbors']]
+
+# merge tables
+merged_scores = pandas.merge(hg_table, mm_table, on="sequence", suffixes=('_hg',"_mm"))
+
+# associate exon targeted with each guide
+
+# split (on targeted exon), select top 4 (if possible) min(sum_hamming_mm + sum_hamming_hg)??
+
+
+
