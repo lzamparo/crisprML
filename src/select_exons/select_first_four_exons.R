c6e369859a03658f5fe598bbf484a3a401354068
Lee Zamparo <zamparo@gmail.com>
2017-11-19 18:01:58 -0500

    selected exons, done.

--- src/select_exons/select_first_four_exons.R
+++ src/select_exons/select_first_four_exons.R
@@ -29,18 +29,4 @@ setkey(gene_names_dt, ensembl_transcript_id)
 coding_exons_gene_names_dt = merge(res_dt, gene_names_dt, by.x=c("ensembl_transcript_id"), by.y=c("ensembl_transcript_id"))
 
 first_four_coding_exons_by_tx_dt = coding_exons_gene_names_dt[,  head(.SD[order(exon_chrom_start)],4), by=.(ensembl_transcript_id)]
-
-#d <- data.table(mtcars, key="cyl")
-#d[, head(.SD[order(disp)], 3), by=cyl]
-
-grouped_filtered_guides_to_remaining_dt = filtered_guides_to_remaining_exons[, .SD[which.min(score_summary)], by=.(gene, transcript, exon_number)]
-
-
-exons = data.table(read.delim("mm10_annotation_cds_only_coding_exons_sorted_unique_bygene_exon.bed", sep="\t", header = FALSE))
-colnames(exons) = c("chrom", "start", "end", "gene", "transcript", "exon_num", "max_exons")
-
-first_four_exons <- exons %>% group_by(gene) %>% top_n(-4, exon_num)
-first_four = as.data.table(ungroup(first_four_exons))
-
-ff_exons = as.data.table(ungroup(first_five_exons))
-write.table(ff_exons, file="mm10_first_five_exons.bed", sep="\t", row.names=FALSE, quote=FALSE)
+write.csv(first_four_coding_exons_by_tx_dt, "mm10_first_four_coding_exons_ensembl_forreal.csv",row.names=FALSE)
