583281334ea4adc30d51334717def9450ed54176
Lee Zamparo <zamparo@gmail.com>
2017-12-08 19:41:33 -0500

    added scripts to take only first two exons, plus to extract those exs from txs in the Doench brie library

--- src/filter_guides_library_first_two_exons.R
+++ src/filter_guides_library_first_two_exons.R
@@ -6,8 +6,6 @@ library(dplyr)
 
 ### Get the input for guides vs mm10 and hg19
 setwd("/Users/zamparol/projects/crisprML/data")
-
-#guide_features_mm10 = data.table(read.csv("guides/raw_features_computed_Cas9_sequences_vs_mm10.csv"))
 guide_features_mm10 = data.table(fread("guides/raw_features_computed_Cas9_sequences_vs_mm10.csv"))
 guide_features_mm10 = guide_features_mm10[,.(sequence, Specificity_Score,Occurrences_at_Hamming_0,Occurrences_at_Hamming_1,Occurrences_at_Hamming_2,Occurrences_at_Hamming_3)]
 guide_features_hg19 = data.table(fread("guides/raw_features_computed_Cas9_sequences_vs_hg19.csv"))
@@ -16,6 +14,19 @@ guide_features_hg19 = guide_features_hg19[,.(sequence, Specificity_Score,Occurre
 ### Get the list of exons that actually overlap guide regions
 coding_exons = data.table(fread("coding_exons/mm10_first_four_coding_exons_ensembl.bed", sep="\t", header=TRUE))
 colnames(coding_exons) = c("chrom","start","end","strand","exon","transcript","gene", "transcript_start", "transcript_end")
+
+### Select the first two coding exons from each trancripts
+first_two_coding_exons = coding_exons[, .SD[order(start)][1:2,], by=.(transcript)]
+
+### the above works, but misses out on those transcripts which are mono-exonic
+mono_exonic_txs = coding_exons[transcript %in% first_two_coding_exons[is.na(chrom) & is.na(start) & is.na(end),transcript], ]
+first_two_coding_exons = first_two_coding_exons[!is.na(chrom),]
+first_two_coding_exons = data.table(rbind(first_two_coding_exons,mono_exonic_txs))
+
+### annotate how many exons are shared by > 1 tx
+first_two_coding_exons[,exon_count := .N,by = exon]
+# first_two_coding_exons[,.N] - first_two_coding_exons[exon_count > 1, .N]
+#[1] 131518
 
 ### Get the map from guides to genes
 guides_to_targets = data.table(fread("guides/guide_to_target_region.csv", header=FALSE))
@@ -28,39 +39,23 @@ feature_tables = merge(x=guide_features_hg19, y=guide_features_mm10, by.x="seque
 filtered_guides = feature_tables[Occurrences_at_Hamming_0.hg == 0 & Occurrences_at_Hamming_1.hg == 0 & Occurrences_at_Hamming_2.hg == 0 & Occurrences_at_Hamming_0.mm == 1 & Occurrences_at_Hamming_1.mm == 0 & Occurrences_at_Hamming_2.mm == 0,]
 
 ### tidy up environment
-rm(guide_features_mm10, guide_features_hg19)
+rm(guide_features_mm10, guide_features_hg19, coding_exons)
 
 ### Join filtered guides targeted gene, transcript, exon, retain info re: human and mouse
 setkey(guides_to_targets, chrom, start, end)
-setkey(coding_exons, chrom, start, end)
-guides_to_exons = foverlaps(guides_to_targets, coding_exons, type="within", nomatch=0L)
-
+setkey(first_two_coding_exons, chrom, start, end)
+guides_to_exons = foverlaps(guides_to_targets, first_two_coding_exons, type="within", nomatch=0L)
 
 ### Keep only those guides that are part of the filtered guides set
 setkey(filtered_guides, sequence)
 setkey(guides_to_exons, guide)
 filtered_guides_and_targets = merge(filtered_guides, guides_to_exons, by.x=c("sequence"), by.y=c("guide"))
-saveRDS(filtered_guides_and_targets, file="../results/library/more_specific_guides_and_targets.rds")
+setnames(filtered_guides_and_targets, c("i.start", "i.end"), c("guide_start", "guide_end"))
+#saveRDS(filtered_guides_and_targets, file="../results/library/more_specific_guides_and_targets.rds")
 
-### What is going on??  How can I have so many guides per gene, exon, exon number??
-filtered_guides_and_targets[, num_guides := .N, by = .(gene, transcript, exon)]
+### How many guides target each exon?
+filtered_guides_and_targets[, num_guides := .N, by = .(exon)]
 
 ### What about just the guide which targets each exon, but with minimal Hamming_3 in mm?
 zug = filtered_guides_and_targets[, .SD[which.min(Occurrences_at_Hamming_3.mm)], by = .(exon)]
-ufgs = unique(filtered_guides_and_targets[ , .(num_guides), by = .(gene, transcript, exon)])
-
-### How many (gene, transcript, exon) records do not have guides?
-setkey(coding_exons, gene, transcript, exon)
-setkey(ufgs, gene, transcript, exon)
-merged_w_guides = merge(ufgs, coding_exons, all.y=TRUE)
-
-### How many (gene, transcript, exon) records have guides?
-ggplot(ufgs, aes(x=num_guides)) + 
-  geom_bar() + 
-  xlim(0,50) + 
-  ggtitle("Number of high quality guides per gene,transcript,exon") + 
-  xlab("Number of guides") + 
-  ylab("count") + 
-  annotate("text", x = 40, y = 20000, label = paste("(g,t,e) hit:", ufgs[,.N], sep=" ")) + 
-  annotate("text", x = 41, y = 15000, label = paste("(g,t,e) not hit:", merged_w_guides[is.na(num_guides), .N], sep=" "))
 
---
+++ src/select_exons/select_brie_tx_exons.R
@@ -0,0 +1,33 @@
+library(data.table)
+library(ggplot2)
+library(biomaRt)
+
+### extract the list of transcripts targeted by Doench in the Brie library, 
+### keep only those exons belonging to that set, write out the
+### set of first two or first four coding exons of each
+
+setwd("/Users/zamparol/projects/crisprML/data")
+
+### Get our table of first four coding exons for all txs, and the Brie list of txs
+coding_exons = data.table(fread("coding_exons/mm10_first_four_coding_exons_ensembl.bed", sep="\t", header=TRUE))
+colnames(coding_exons) = c("chrom","start","end","strand","exon","transcript","gene", "transcript_start", "transcript_end")
+doench_guides = data.table(fread("coding_exons/broadgpp-brie-library-contents.txt", sep="\t", header=TRUE))
+doench_guides[, transcript := tstrsplit(Target.Transcript, ".", fixed=TRUE)[[1]]]
+
+### get the ENSEMBL transcript codes for the Brie RefSeq txs
+mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+txs = as.character(unlist(doench_guides[!duplicated(transcript),transcript]))
+res = getBM(attributes=c("refseq_mrna", "ensembl_transcript_id"),
+            filters = c("refseq_mrna"), 
+            values = txs, 
+            mart = mouse)
+res_dt = data.table(res)
+
+
+### keep only the exons int the res_dt txs set
+setkey(res_dt, transcript)
+setkey(coding_exons, transcript)
+filtered_txs = coding_exons[transcript %in% unique(res_dt[, ensembl_transcript_id]),]
+
+### keep only the relevant stuff
+write.table(filtered_txs, file = "coding_exons/mm10_first_four_filtered_brie_exons.bed", sep="\t", quote=FALSE, row.names=FALSE)
