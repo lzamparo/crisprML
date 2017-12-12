db167cb7fd96ac63dac1c095c998f549c026a600
Lee Zamparo <zamparo@gmail.com>
2017-12-12 11:09:25 -0500

    reorganized order of calculation to mirror how difference is calculated

--- src/guides_io_comparison.sh
+++ src/guides_io_comparison.sh
@@ -4,6 +4,7 @@
 
 
 # Go to directory with inputs, count up number of scored guides
+echo "calculating total number of input guides..."
 curr=`pwd`
 cd $1
 input_totals=$(find . -name "*.txt" -exec wc -l {} \; | cut -f1 -d' ' | awk '{ sum += $1 } END { print sum }')
@@ -14,6 +15,7 @@ echo "Found $input_totals guides as input"
 prefix=$(dirname $1)
 suffix=$(basename $1 | sed -e 's/input/output/')
 cd $prefix/$suffix
+echo "calculating total number of output guides..."
 output_totals=$(find . -name "*.csv" -exec wc -l {} \; | cut -f1 -d' ' | awk '{ sum += $1 } END { print sum }')
 echo "Found $output_totals guides as output"
 
