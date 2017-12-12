c8c8b5d6674384995289e232f38acb70e404a5aa
Lee Zamparo <zamparo@gmail.com>
2017-12-12 11:07:58 -0500

    reorganized order of calculation to mirror how difference is calculated

--- src/guides_io_comparison.sh
+++ src/guides_io_comparison.sh
@@ -2,21 +2,21 @@
 
 ### identify experiments that did not produce data for all input guides
 
-# Go to directory with outputs, count up number of scored guides
+
+# Go to directory with inputs, count up number of scored guides
 curr=`pwd`
 cd $1
+input_totals=$(find . -name "*.txt" -exec wc -l {} \; | cut -f1 -d' ' | awk '{ sum += $1 } END { print sum }')
+echo "Found $input_totals guides as input"
 
-output_totals=$(find . -name "*.csv" -exec wc -l {} \; | cut -f1 -d' ' | awk '{ sum += $1 } END { print sum }')
 
 # Go to directory with outputs, count up number of scored guides
 prefix=$(dirname $1)
-suffix=$(basename $1 | sed -e 's/output/input/')
+suffix=$(basename $1 | sed -e 's/input/output/')
 cd $prefix/$suffix
-
-input_totals=$(find . -name "*.txt" -exec wc -l {} \; | cut -f1 -d' ' | awk '{ sum += $1 } END { print sum }')
-
-disc=$(echo "$input_totals - $output_totals"  | bc -l )
-
-echo "Found $input_totals guides as input"
+output_totals=$(find . -name "*.csv" -exec wc -l {} \; | cut -f1 -d' ' | awk '{ sum += $1 } END { print sum }')
 echo "Found $output_totals guides as output"
+
+# print difference found
+disc=$(echo "$input_totals - $output_totals"  | bc -l )
 echo "Discrepancy is $disc"
